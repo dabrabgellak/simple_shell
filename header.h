5046be8fa31b292e327aa36e4a8da5d874c569b0
nataliamedinat <nataliamedinabotero@gmail.com>
2019-08-20 21:29:21 +0000

    header file

---
+++ header.h
@@ -0,0 +1,14 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+void shell_loop(void);
+int *read_line(void);
+
+#endif
