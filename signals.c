13e7053c6cb530da5b7c7e48d519283bd0237afc
nataliamedinat <nataliamedinabotero@gmail.com>
2019-08-29 17:24:37 +0000

    Betty happy

--- signals.c
+++ signals.c
@@ -1,13 +1,13 @@
 #include "header.h"
 /**
- * sigint_handler - Signal handler.
+ * control_signal - Signal handler.
  * @sig: signal.
  */
 
 void control_signal(int sig)
 {
 	(void)sig;
-	write(STDOUT_FILENO,"\n", 1);
+	write(STDOUT_FILENO, "\n", 1);
 	write(STDOUT_FILENO, "$ ", 2);
 	fflush(stdout);
 
